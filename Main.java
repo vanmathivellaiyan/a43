c5f63eb559f7f8a303162dc637b292baf5007cc3
vanmathivellaiyan <vanmathivellaiyan1995@gmail.com>
2016-07-30 18:29:31 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package guviprogram;
+public class Main {
+
+  public static boolean isNumeric(String string) {
+      return string.matches("^[-+]?\\d+(\\.\\d+)?$");
+  }
+  
+  
+  public static void main(String[] args) {
+sysout isNumeric("123.123")
+  }
+}
